b8d85d805e756db9c12da60470e0037c8ca9dd3c
Patrick Lawrence <patrick.lawrence@gmail.com>
2020-05-02 18:43:08 -0700

    Update aliasman to v1.0.0

--- aliasman.rb
+++ aliasman.rb
@@ -1,10 +1,10 @@
 class Aliasman < Formula
     desc "Alias manager -- manage large numbers of email aliases"
     homepage "https://github.com/patsoffice/aliasman"
-    url "https://github.com/patsoffice/aliasman/releases/download/v1.0.0-beta.2/aliasman_darwin_amd64"
-    version "v1.0.0-beta.2"
-    sha256 "0fcecc0f6c427b303be9ba428cae3777eeff2d65431ebf77b659c9f05078cdaf"
-  
+    url "https://github.com/patsoffice/aliasman/releases/download/v1.0.0/aliasman_darwin_amd64"
+    version "v1.0.0"
+    sha256 "2042d70bc640f3cf404f0d56237cf9ab28421c0b32e85cebab9265c70d6dab80"
+
     def install
       bin.install 'aliasman_darwin_amd64'
       mv bin/"aliasman_darwin_amd64", bin/"aliasman"
