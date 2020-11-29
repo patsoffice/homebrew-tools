40c60b50e21baeadaf95d79a905d451d76bf0a78
Patrick Lawrence <patrick.lawrence@gmail.com>
2020-11-29 08:22:04 -0800

    Update app-quarantine to v1.0.2

--- app-quarantine.rb
+++ app-quarantine.rb
@@ -1,9 +1,9 @@
 class AppQuarantine < Formula
     desc "app-quarantine -- A tool to identify and fix app quarantine issues on macOS"
     homepage "https://github.com/patsoffice/app-quarantine"
-    url "https://github.com/patsoffice/app-quarantine/releases/download/v1.0.1/app-quarantine_darwin_amd64"
-    version "v1.0.1"
-    sha256 "74d2efb26b3157230e0742a609921c66d637087f509c317a5c9f2148b6ace617"
+    url "https://github.com/patsoffice/app-quarantine/releases/download/v1.0.2/app-quarantine_darwin_amd64"
+    version "v1.0.2"
+    sha256 "cf66d889bac95d8fb58c6f17b78e25a5f2604e530bbe81f8d59dedc9920436bc"
 
     def install
       bin.install 'app-quarantine_darwin_amd64'
